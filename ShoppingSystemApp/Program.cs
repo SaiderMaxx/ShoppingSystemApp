426d604248e81684a160457aae21f1906d1c91fe
WIKSATA1337 <ciganskigeigerwiksa26g@gmail.com>
2022-03-09 16:26:29 +0200

    Adding class Product. Example in main.

--- ShoppingSystemApp/Program.cs
+++ ShoppingSystemApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystemApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main
+
+            // Product ToString() works this way:
+            // Product product = new Product("test", 2);
+            // Console.WriteLine(product.ToString());
+            // Output:
+            // Name: test
+            // Price: 2
+        }
+    }
+}
